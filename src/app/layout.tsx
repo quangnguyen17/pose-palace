5aadf0faaa15a40e4c1fb91b65769f04da18754c
Quang Nguyen <wan15112001@gmail.com>
2024-04-09 19:32:36 -0700

    calendly iframe

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -3,15 +3,13 @@ import './global.css'
 
 export const metadata: Metadata = {
   title: 'Pose Palace',
-  description:
-    'Unlock Your Creative Potential at Pose Palace: Where Photos and Videos Come to Life!',
+  description: '',
 }
 
 export default function RootLayout({ children }: Readonly<{ children: React.ReactNode }>) {
   return (
     <html lang="en">
       <body>{children}</body>
-      <footer>Crystal Lighthouse LLC</footer>
     </html>
   )
 }
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,24 +1,3 @@
-import type { FC } from 'react'
-import Link from 'next/link'
-
-const App: FC = () => {
-  return (
-    <div style={{ display: 'flex', padding: '1rem' }}>
-      <Link
-        href="/schedule"
-        className="button-outlined"
-        style={{
-          flex: 1,
-          width: '100%',
-          height: '100%',
-          textDecoration: 'none',
-          textAlign: 'center',
-        }}
-      >
-        Make an appointment @ Pose Palace
-      </Link>
-    </div>
-  )
+export default function App() {
+  return <iframe src="https://calendly.com/posepalace/15min"></iframe>
 }
-
-export default App
