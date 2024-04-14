42db1b09e8f02dc1a8b318015efdf934ce3e3cdd
Quang Nguyen <wan15112001@gmail.com>
2024-04-14 11:57:17 -0700

    new SEO

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,11 +1,13 @@
 import type { Metadata } from 'next'
 
+const description: string =
+  'Step into our self-serve photo studio and unlock a world of creativity! From candid moments to professional portraits, our space is your canvas. Capture memories, express yourself, and bring your vision to life with ease. With a user-friendly environment designed for seamless shooting, every photo session is a masterpiece in the making. Discover the freedom to capture your moments your way. Your memories, your style – all in one place!'
+
 export const metadata: Metadata = {
   title: 'Pose Palace Studio',
   creator: 'Pose Palace Studio',
   keywords: ['Pose Palace', 'Pose Palace Studio', 'self photo', 'self portrait', 'photo taking'],
-  description:
-    'Step into our self-serve photo studio and unlock a world of creativity! From candid moments to professional portraits, our space is your canvas. Capture memories, express yourself, and bring your vision to life with ease. With a user-friendly environment designed for seamless shooting, every photo session is a masterpiece in the making. Discover the freedom to capture your moments your way. Your memories, your style – all in one place!',
+  description,
   authors: [
     {
       name: 'Pose Palace Studio',
@@ -24,6 +26,36 @@ export const metadata: Metadata = {
       url: 'https://www.instagram.com/crystalnguyennnn/',
     },
   ],
+  icons: {
+    icon: 'https://posepalace.com/logo.jpg',
+    apple: 'https://posepalace.com/logo.jpg',
+  },
+  twitter: {
+    card: 'summary_large_image',
+    site: 'https://posepalace.com/',
+    creator: '@posepalacestudio',
+    images: 'https://posepalace.com/logo.jpg',
+  },
+  openGraph: {
+    type: 'website',
+    url: 'https://www.posepalace.com/',
+    title: 'Pose Palace Studio',
+    description,
+    siteName: 'Pose Palace Studio',
+    images: [
+      { url: 'https://posepalace.com/logo.jpg', username: '@posepalacestudio' },
+      { url: 'https://posepalace.com/q1.jpg', username: '@quang_dng20' },
+      { url: 'https://posepalace.com/q2.jpg', username: '@quang_dng20' },
+      { url: 'https://posepalace.com/q3.jpg', username: '@quang_dng20' },
+      { url: 'https://posepalace.com/q4.jpg', username: '@quang_dng20' },
+    ],
+  },
+  applicationName: 'Pose Palace Studio',
+  appleWebApp: {
+    capable: true,
+    title: 'Pose Palace Studio',
+    statusBarStyle: 'default',
+  },
 }
 
 export default function RootLayout({ children }: Readonly<{ children: React.ReactNode }>) {
