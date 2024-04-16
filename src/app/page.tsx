fe040f8a6c19c8596ec934316ae65ecbde2ccea7
Quang Nguyen <wan15112001@gmail.com>
2024-04-15 21:10:19 -0700

    remove icon

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -33,7 +33,7 @@ const App: FC = () => {
       </div>
       <footer>
         <a href="https://calendly.com/posepalace/15min" target="_blank">
-          🌉 Click here to schedule your self photo session today at Pose Palace Studio!
+          [+] Click here to schedule your self photo session today at Pose Palace Studio!
         </a>
       </footer>
     </div>
