a7be6ed36ab140869af391b3d1ec3fa05e5e4a85
Quang Nguyen <wan15112001@gmail.com>
2024-04-19 18:20:57 -0700

    revert to redirect

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,56 +1,9 @@
 import type { Metadata } from 'next'
-import './global.css'
-
-const description: string =
-  'Step into our self-serve photo studio and unlock a world of creativity! From candid moments to professional portraits, our space is your canvas. Capture memories, express yourself, and bring your vision to life with ease. With a user-friendly environment designed for seamless shooting, every photo session is a masterpiece in the making. Discover the freedom to capture your moments your way. Your memories, your style – all in one place!'
 
 export const metadata: Metadata = {
   title: 'Pose Palace Studio',
   creator: 'Pose Palace Studio',
   keywords: ['Pose Palace', 'Pose Palace Studio', 'self photo', 'self portrait', 'photo taking'],
-  description,
-  authors: [
-    {
-      name: 'Pose Palace Studio',
-      url: 'https://www.instagram.com/posepalacestudio/',
-    },
-    {
-      name: 'Quang Nguyen',
-      url: 'https://www.instagram.com/quang_dng20/',
-    },
-    {
-      name: 'Dang Nguyen',
-      url: 'https://www.instagram.com/dangh_nguyen/',
-    },
-    {
-      name: 'Crystal Nguyen',
-      url: 'https://www.instagram.com/crystalnguyennnn/',
-    },
-  ],
-  icons: {
-    icon: 'https://posepalace.com/icon.jpg',
-    apple: 'https://posepalace.com/icon.jpg',
-  },
-  twitter: {
-    card: 'summary_large_image',
-    site: 'https://posepalace.com/',
-    creator: '@posepalacestudio',
-    images: 'https://posepalace.com/icon.jpg',
-  },
-  openGraph: {
-    type: 'website',
-    url: 'https://www.posepalace.com/',
-    title: 'Pose Palace Studio',
-    description,
-    siteName: 'Pose Palace Studio',
-    images: [{ url: 'https://posepalace.com/icon.jpg', username: '@posepalacestudio' }],
-  },
-  applicationName: 'Pose Palace Studio',
-  appleWebApp: {
-    capable: true,
-    title: 'Pose Palace Studio',
-    statusBarStyle: 'default',
-  },
 }
 
 export default function RootLayout({ children }: Readonly<{ children: React.ReactNode }>) {
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,53 +1,5 @@
-const photos: string[] = [
-  'c1.jpg',
-  'c10.jpg',
-  'c2.jpg',
-  'c3.jpg',
-  'c4.jpg',
-  'c5.jpg',
-  'c6.jpg',
-  'c7.jpg',
-  'c8.jpg',
-  'c9.jpg',
-  'd1.jpg',
-  'd2.jpg',
-  'q12.jpg',
-  'q13.jpg',
-  'q14.jpg',
-  'q15.jpg',
-  'q2.jpg',
-  'q7.jpg',
-  'q8.jpg',
-  'q9.jpg',
-]
+'use server'
 
-const hashtags: string[] = ['posepalacestudio', 'full-body', 'portraits', 'goofy']
+import { redirect } from 'next/navigation'
 
-export default function App() {
-  return (
-    <div className="App">
-      <header>
-        <a className="instagram" href="https://www.instagram.com/posepalacestudio/" target="_blank">
-          @posepalacestudio
-        </a>
-        {hashtags.map((hashtag, idx) => (
-          <a key={idx} href={`https://www.instagram.com/explore/tags/${hashtag}`} target="_blank">
-            #{hashtag}
-          </a>
-        ))}
-      </header>
-      <div className="grid-container">
-        {photos.map((imagePath, idx) => (
-          <div className="grid-item" key={idx}>
-            <img src={`/photos/${imagePath}`} alt="pose palace studio collection" loading="lazy" />
-          </div>
-        ))}
-      </div>
-      <footer>
-        <a href="https://calendly.com/posepalace/15min" target="_blank">
-          🗓️ Click here to schedule your self photo session today at Pose Palace Studio
-        </a>
-      </footer>
-    </div>
-  )
-}
+export default async () => redirect('https://calendly.com/posepalace/15min')
