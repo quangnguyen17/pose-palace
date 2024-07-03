b2f2c248498f468233f9137278233331a8c8dccb
Quang Nguyen <wan15112001@gmail.com>
2024-07-03 15:01:45 -0700

    add flickering logo

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,10 +2,14 @@
 
 const Page = () => {
   return (
-    <div
-      className="App"
-      onClick={() => window.open('https://www.instagram.com/posepalacestudio/')}
-    ></div>
+    <div className="App align-items-center tracking-in-expand-fwd">
+      <a className="poster" href="https://www.instagram.com/posepalacestudio/" target="_blank">
+        <img src="opening-soon.jpg" alt="opening-soon.jpg" />
+      </a>
+      <div className="flickering-rainbow-border">
+        <img className="logo" src="logo.png" alt="logo.png" />
+      </div>
+    </div>
   )
 }
 
