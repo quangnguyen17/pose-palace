da19cad40bc5b648ad75dba4c48d6ca5121d957b
Quang Nguyen <wan15112001@gmail.com>
2024-07-05 23:29:20 -0700

    add address and instructions

--- src/app/components/Footer.tsx
+++ src/app/components/Footer.tsx
@@ -9,19 +9,45 @@ export const Footer = () => {
         padding: '1rem',
         gap: '1rem',
         display: 'flex',
-        flexDirection: 'row',
+        flexDirection: 'column',
         justifyContent: 'flex-start',
         alignItems: 'flex-start',
         flexWrap: 'wrap',
         fontSize: '15px',
-        lineHeight: '20px',
+        lineHeight: '24px',
       }}
     >
-      <p>© 2024 POSE PALACE self-portrait studio</p>
-      <Link href="/gallery">explore our work</Link>
-      <Link href="https://www.instagram.com/posepalacestudio/" target="_blank">
-        @posepalacestudio
-      </Link>
+      <p style={{ color: 'black' }}>
+        <span style={{ fontStyle: 'italic' }}>
+          (We are located inside the AR Galleria, on the left-hand side of Tous Les Jours)
+        </span>
+        <Link
+          href="http://maps.google.com/maps?q=9618+Garden+Grove+Blvd%2C+Garden+Grove%2C+CA+92844"
+          target="_blank"
+          style={{ textAlign: 'left', color: 'black' }}
+        >
+          9618 Garden Grove Blvd # 106
+          <br />
+          Garden Grove, CA 92844
+        </Link>
+        <Link href="tel:714-467-7036" style={{ textAlign: 'left', color: 'black' }}>
+          (714) 467-7036
+        </Link>
+      </p>
+      <p>
+        <Link href="/gallery" style={{ textAlign: 'left' }}>
+          explore our work
+        </Link>
+        <Link
+          href="https://www.instagram.com/posepalacestudio/"
+          target="_blank"
+          style={{ textAlign: 'left' }}
+        >
+          @posepalacestudio
+        </Link>
+        <span></span>
+      </p>
+      <p style={{ color: 'rgb(100, 100, 100)' }}>© 2024 POSE PALACE self-portrait studio</p>
     </footer>
   )
 }
--- src/app/components/Header.tsx
+++ src/app/components/Header.tsx
@@ -8,37 +8,23 @@ export const Header = () => {
         width: '100%',
         padding: '1rem',
         display: 'flex',
-        flexDirection: 'row',
-        justifyContent: 'space-between',
+        flexDirection: 'column',
+        justifyContent: 'center',
         alignItems: 'center',
         flexWrap: 'wrap',
         borderBottom: '1px solid rgb(230, 230, 230)',
       }}
     >
-      <Link href="/">
+      <Link href="/" style={{ display: 'flex', justifyContent: 'center', alignItems: 'center' }}>
         <Image
           src="/logo.png"
           alt="Pose Palace Logo"
-          width={150}
-          height={65}
+          width={200}
+          height={100}
           unoptimized
           style={{ objectFit: 'contain' }}
         />
       </Link>
-      <p style={{ textAlign: 'right', fontSize: '14px', lineHeight: '20px' }}>
-        <Link
-          href="http://maps.google.com/maps?q=9618+Garden+Grove+Blvd%2C+Garden+Grove%2C+CA+92844"
-          target="_blank"
-          style={{ textAlign: 'right', color: 'black' }}
-        >
-          9618 Garden Grove Blvd # 106
-          <br />
-          Garden Grove, CA 92844
-        </Link>
-        <Link href="tel:714-467-7036" style={{ textAlign: 'right', color: 'black' }}>
-          (714) 467-7036
-        </Link>
-      </p>
     </header>
   )
 }
