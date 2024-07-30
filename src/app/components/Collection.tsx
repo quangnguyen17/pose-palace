ac8bccbc40a4bf94b8062a111455c024983870ed
Quang Nguyen <wan15112001@gmail.com>
2024-07-29 22:44:54 -0700

    new collection header

--- src/app/components/Collection.tsx
+++ src/app/components/Collection.tsx
@@ -10,37 +10,37 @@ type CollectionProps = {
 }
 
 export const Collection: FC<CollectionProps> = ({ type, room, photos }) => {
-  const tintColor = room.toLowerCase().includes('white') ? 'black' : 'white'
   return (
     <Fragment>
+      <div className="headline">
+        <p>
+          <span className="type">{type}</span> <span className="room">{room}</span>
+        </p>
+        <Link
+          href="https://www.instagram.com/posepalacestudio/"
+          target="_blank"
+          style={{
+            display: 'flex',
+            flexDirection: 'column',
+            justifyContent: 'center',
+            alignItems: 'center',
+          }}
+        >
+          <img src="/instagram.png" alt="/instagram.png" className="instagram" />
+        </Link>
+      </div>
       <div className="collection">
         {photos.map((photo) => (
-          <div key={photo} className="photo-wrapper">
-            <Image
-              src={photo}
-              alt={photo}
-              quality={100}
-              width={100}
-              height={100}
-              unoptimized
-              className="photo"
-            />
-            <div className="headline">
-              <p className="type" style={{ color: tintColor }}>
-                {type}
-              </p>
-              <p className="room" style={{ color: tintColor }}>
-                {room}
-              </p>
-            </div>
-            <Link href="https://www.instagram.com/posepalacestudio/" target="_blank">
-              <img
-                src="/instagram.png"
-                alt="/instagram.png"
-                className={`instagram${tintColor === 'white' ? ' invert-color' : ''}`}
-              />
-            </Link>
-          </div>
+          <Image
+            key={photo}
+            src={photo}
+            alt={photo}
+            quality={100}
+            width={100}
+            height={100}
+            unoptimized
+            className="photo"
+          />
         ))}
       </div>
     </Fragment>
