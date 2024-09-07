5ab2444c4cf31627e9b48b79aa7eb750c8f2a5d8
Quang Nguyen <wan15112001@gmail.com>
2024-09-07 12:01:24 -0700

    add 30 minutes option

--- src/app/book-now/page.tsx
+++ src/app/book-now/page.tsx
@@ -1,4 +1,5 @@
 import { FC } from 'react'
+import { Page } from '../components/Page'
 import Link from 'next/link'
 import './book-now.css'
 
@@ -19,17 +20,27 @@ const sessions = [
     title: '$67 • 15-Minute @ Full Body White Room',
     link: 'https://calendly.com/posepalacestudio/15min',
   },
+  {
+    title: '$100 • 30-Minute @ Standard Color Room',
+    link: 'https://calendly.com/posepalacestudio-colorroom/30min',
+  },
+  {
+    title: '$100 • 30-Minute @ Full Body White Room',
+    link: 'https://calendly.com/posepalacestudio/30min',
+  },
 ]
 
 const BookNow: FC = () => {
   return (
-    <div className="sessions">
-      {sessions.map((session, index) => (
-        <Link key={index} href={session.link} className="session">
-          <h1>{session.title}</h1>
-        </Link>
-      ))}
-    </div>
+    <Page gap="1rem" padding="0">
+      <div className="sessions">
+        {sessions.map((session, index) => (
+          <Link key={index} href={session.link} className="session">
+            <h1>{session.title}</h1>
+          </Link>
+        ))}
+      </div>
+    </Page>
   )
 }
 
