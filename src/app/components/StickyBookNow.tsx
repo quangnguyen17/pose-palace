c2732e9722dd5afe6483e02dbfd55973e4d38815
Quang Nguyen <wan15112001@gmail.com>
2025-01-11 23:12:43 -0800

    redesigned book now

--- src/app/components/StickyBookNow.tsx
+++ src/app/components/StickyBookNow.tsx
@@ -1,4 +1,5 @@
 import Link from 'next/link'
+import Image from 'next/image'
 
 export const StickyBookNow = () => {
   return (
@@ -6,14 +7,25 @@ export const StickyBookNow = () => {
       style={{
         backgroundColor: 'black',
         color: 'white',
-        fontSize: '15px',
-        fontWeight: 500,
-        padding: '15px',
+        fontSize: '16px',
+        padding: '14px 16px',
+        display: 'flex',
+        flexDirection: 'row',
+        justifyContent: 'space-between',
+        alignItems: 'center',
       }}
       href="/book-now"
       className="sticky-book-now"
     >
-      BOOK NOW
+      <span>Book Now</span>
+      <Image
+        style={{ filter: 'invert(1)' }}
+        src="/right-arrow.png"
+        alt="/right-arrow.png"
+        width={26}
+        height={26}
+        unoptimized
+      />
     </Link>
   )
 }
