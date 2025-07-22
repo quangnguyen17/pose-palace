191b20f6c1457dccb904735c4e36877948719ea5
Quang Nguyen <wan15112001@gmail.com>
2025-07-22 00:08:29 -0700

    clean up about page

--- src/app/components/Home.tsx
+++ src/app/components/Home.tsx
@@ -62,42 +62,39 @@ export const Home = () => (
       <p>{`🪖 Military Personnel`}</p>
       <p>{`🏥 Healthcare Workers`}</p>
     </Card>
-    <Card style={{ background: '#dee7e7' }}>
-      <h3>How it works</h3>
-      <p>
-        Our studio consists of two rooms: one features a cyclorama white wall, ideal for wide,
-        full-body shots, while the other offers multiple color backdrops, perfect for more personal
-        waist-up to headshot portraits. An on-site attendant will guide you into either room,
-        provide you with a remote clicker to trigger the camera shutter, assist you in taking your
-        first shot to familiarize you with the equipment, and finally, start the timer for your
-        booked session.
-      </p>
-    </Card>
-    <Card style={{ background: '#dee7e7' }}>
-      <h3>Our equipments</h3>
-      <p>1 Sony Full-Frame camera</p>
-      <p>1 32-inch monitor displaying the camera's live view/feed</p>
-      <p>1 27-inch monitor displaying the last photo taken</p>
-      <p>1-2 photography strobe lights depending on the room.</p>
-    </Card>
-    <Card style={{ background: '#dee7e7' }}>
-      <h3>Props available for use:</h3>
-      <p>
-        Benches, cubes, chairs, stools, seasonal and party props like glasses, headbands, hats, etc.
-      </p>
-    </Card>
-    <Card style={{ background: '#dee7e7' }}>
-      <p>
-        We are a proud Vietnamese-American-owned self-portrait photo studio conveniently located in
-        Koreatown, Garden Grove. Our mission is to provide professional photos at affordable prices.
-        You might wonder what a self-portrait studio is. Essentially, it's a photography studio
-        where customers take photos of themselves, rather than being photographed by a professional.
-        Equipped with a 32-inch monitor displaying the camera's live feed, you can see yourself
-        exactly as you appear in the camera, much like looking into a mirror. We firmly believe that
-        capturing memorable moments is invaluable. What better way to preserve these moments than by
-        capturing them yourself, with your own authentic expressions and feelings?
-      </p>
-      <p>© 2024 Crystal Lighthouse LLC</p>
-    </Card>
+    <div className="divider"></div>
+    <h4>How It Works</h4>
+    <p>
+      Our studio consists of two rooms: one features a cyclorama white wall, ideal for wide,
+      full-body shots, while the other offers multiple colored backdrops, perfect for more personal
+      waist-up or headshot portraits. An on-site attendant will guide you into either room, provide
+      you with a remote clicker to trigger the camera shutter, assist you in taking your first shot
+      to familiarize you with the equipment, and finally start the timer for your booked session.
+    </p>
+    <h4>Our Equipment:</h4>
+    <p>
+      1 Sony full-frame camera
+      <br />1 32-inch monitor displaying the camera's live view/feed
+      <br />1 27-inch monitor displaying the last photo taken
+      <br />
+      1-2 photography strobe lights, depending on the room
+    </p>
+    <h4>Props Available for Use:</h4>
+    <p>
+      Benches, cubes, chairs, stools, and seasonal or party props such as glasses, headbands, hats,
+      etc.
+    </p>
+    <h4>About Us</h4>
+    <p>
+      We are a proud Vietnamese-American-owned self-portrait photo studio conveniently located in
+      Koreatown, Garden Grove. Our mission is to provide professional photos at affordable prices.
+      You might wonder what a self-portrait studio is—essentially, it's a photography studio where
+      customers take photos of themselves rather than being photographed by a professional. Equipped
+      with a 32-inch monitor displaying the camera's live feed, you can see yourself exactly as you
+      appear in the camera, much like looking into a mirror. We firmly believe that capturing
+      memorable moments is invaluable. What better way to preserve these moments than by capturing
+      them yourself, with your own authentic expressions and feelings?
+    </p>
+    <p>© 2024 Crystal Lighthouse LLC</p>
   </Page>
 )
